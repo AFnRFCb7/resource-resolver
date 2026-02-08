5c98650641258f67d46eda3bbff8bfe4fe3b8df5
Emory Merryman <emory.merryman@gmail.com>
2026-02-08 08:40:28 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -221,8 +221,7 @@
                                                                                 done"invalid-init
                                                                                 RELEASE="$( yq eval --prettyPrint ".description.secondary.release <<< "$PAYLOAD" )" || failure dca920f6
                                                                                 # shellcheck disable=2068
-                                                                                echo "$PAYLOAD" | iteration --type init --index "$INDEX" --hash "$HASH" --release "$RELEASE" ${ builtins.concatStringsSep "" [ "$" "{" "INIT_RESOLUTION_ARGS[@]" "}" ] } ${ builtins.concatStringsSep "" [ "$" "{" "RELEASE_RESOLUTION_ARGS[@]" "}" ] } &
-                                                                                true
+                                                                                iteration --type init --index "$INDEX" --hash "$HASH" --release "$RELEASE" ${ builtins.concatStringsSep "" [ "$" "{" "INIT_RESOLUTION_ARGS[@]" "}" ] } ${ builtins.concatStringsSep "" [ "$" "{" "RELEASE_RESOLUTION_ARGS[@]" "}" ] } <<< "$PAYLOAD" &
                                                                             elif [[ "invalid-release" == "$TYPE_" ]]
                                                                             then
                                                                                 HASH="$( yq eval ".hash | tostring " - <<< "$PAYLOAD" )" || failure a22f7da7
